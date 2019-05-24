0e7382b5d6ec1a95a3ea38294f0f22102420e2ea
Diego Carvalho <direisc@gmail.com>
2019-05-24 00:16:41 -0300

    Melhorias no backend

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const routes = express.Router();
 
 const ToolController = require('./controllers/ToolController');
+const OauthController = require('./controllers/OauthController');
 
 routes.post('/tools', ToolController.store);
 routes.get('/tools', ToolController.find);
-routes.delete('/tools/:id', ToolController.remove);
+routes.delete('/tools/:id', OauthController.authenticateRequest, ToolController.remove);
+// routes.delete('/tools/:id', ToolController.remove);
 
 routes.get('/', (req, res) => {
 	return res.send('Sai daqui rapaz!');
--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const OAuth2Server = require('oauth2-server'),
+	Request = OAuth2Server.Request,
+	Response = OAuth2Server.Response;
+
+const OauthController = require('./controllers/OauthController');
+
 const app = express();
 
 app.use(cors());
@@ -17,6 +23,8 @@ mongoose.set('useFindAndModify', false);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.all('/oauth/token', OauthController.obtainToken);
 
 app.use(require('./routes'));
 
