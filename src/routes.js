c540ebf1193583462b36a7222afbd49cac4f0688
Diego Carvalho <direisc@gmail.com>
2019-05-25 13:35:32 -0300

    limpeza do projeto

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,7 @@ const OauthController = require('./controllers/OauthController');
 
 routes.post('/tools', ToolController.store);
 routes.get('/tools', ToolController.find);
-routes.delete('/tools/:id', OauthController.authenticateRequest, ToolController.remove);
-// routes.delete('/tools/:id', ToolController.remove);
+routes.delete('/tools/:id', ToolController.remove);
 
 routes.get('/', (req, res) => {
 	return res.send('Sai daqui rapaz!');
--- src/server.js
+++ src/server.js
@@ -2,12 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const OAuth2Server = require('oauth2-server'),
-	Request = OAuth2Server.Request,
-	Response = OAuth2Server.Response;
-
-const OauthController = require('./controllers/OauthController');
-
 const app = express();
 
 app.use(cors());
@@ -23,8 +17,6 @@ mongoose.set('useFindAndModify', false);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.all('/oauth/token', OauthController.obtainToken);
 
 app.use(require('./routes'));
 
